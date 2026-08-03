91b126a36d43012bb7467bb4503fb6ad58f41fec
ESCANOR7980 <abhishek98834@gmail.com>
2026-08-03 15:15:14 +0530

    Update API URL for Render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ 
-  baseURL: 'https://intellidataops.onrender.com/api'
+const API = axios.create({
+  baseURL: 'https://intellidataops-zbsi.onrender.com/api'
 });
 
 API.interceptors.request.use(c => {
